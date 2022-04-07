c7bbf29b631c340b5a29a113ea2abf7b1303dbcf
Lourdes Roashan <lourdes@woodscamp.com>
2022-04-07 14:32:44 +0530

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <layout>
+    <div class="container">
+      <div class="row align-items-center mt-4">
+        <div class="col-12 mt-4">
+          <div class="card mt-4">
+            <div class="card-body">
+              <BaseStepper></BaseStepper>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <div class="home">
+      <img alt="Vue logo" src="../assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div> -->
+  </layout>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import BaseStepper from '@/components/BaseStepper.vue';
+import layout from '@/components/layouts/default.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    BaseStepper,
+    layout
   }
 }
 </script>
